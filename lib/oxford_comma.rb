9d006ae54eaabd7011ad3467d738abdfd4900825
Osorabrian <osorabrian@gmail.com>
2023-02-07 15:44:14 +0300

    completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
-end
+    if array.length == 1
+        array.join("")
+    elsif array.length == 2
+        "#{array[0]} and #{array[1]}"
+    elsif array.length == 3
+        "#{array[0]}, #{array[1]}, and #{array[2]}"
+    elsif array.length > 3
+        a = array.slice(0,array.length-1)
+        "#{a.join(", ")}, and #{array.last}"
+    end
+end 
